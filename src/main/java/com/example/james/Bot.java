b3e6dc416f6b3a802f9d276de62d103be7eaa375
James Lukaszewski <james.lukaszewski@gmail.com>
2021-06-12 18:31:33 +0200

    "bot class 1.1"

--- src/main/java/com/example/james/Bot.java
+++ src/main/java/com/example/james/Bot.java
@@ -3,5 +3,25 @@ package com.example.james;
 public class Bot {
 	private long id;
 	private String name;
+	
+	public Bot(String name) {
+		setName(name);
+	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
